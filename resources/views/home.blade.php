c816536b46e83f11acaa2242b7df12d677d77b0f
mdnajmul <mdnajmulovi@gmail.com>
2021-05-10 18:53:37 +0600

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row chat-row">
+        <div class="col-md-3">
+            <div class="users">
+                <h5>Users</h5>
+
+                <ul class="list-group list-chat-item">
+                    <li class="chat-user-list">
+                        <a href="">Najmul Ovi</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
+
+        </div>
+    </div>
+@endsection
