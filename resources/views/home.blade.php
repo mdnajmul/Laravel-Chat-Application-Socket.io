5d3c35cf1e648b3727b4fef202b1163ab23592bb
mdnajmul <mdnajmulovi@gmail.com>
2021-05-10 21:18:23 +0600

    basic layout set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,28 @@
                 <h5>Users</h5>
 
                 <ul class="list-group list-chat-item">
-                    <li class="chat-user-list">
-                        <a href="">Najmul Ovi</a>
-                    </li>
+                    @if($users->count())
+                        @foreach($users as $user)
+                            <li class="chat-user-list">
+                                <a href="">
+                                    <div class="chat-image"> 
+                                        {!! makeImageFromName($user->name) !!}
+                                    </div>
+                                    <div class="chat-name"> 
+                                        {{ $user->name }}
+                                    </div>
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
                 </ul>
             </div>
         </div>
         <div class="col-md-9">
-
+            <h1>
+                Message Section
+            </h1>
+            Select user from the list to begin conversation.
         </div>
     </div>
 @endsection
