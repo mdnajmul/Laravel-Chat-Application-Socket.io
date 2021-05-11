6b449aa4b0a56f86a700f1605dda27e68c24c0a9
mdnajmul <mdnajmulovi@gmail.com>
2021-05-11 13:18:03 +0600

    chat body setup

--- resources/views/message/conversation.blade.php
+++ resources/views/message/conversation.blade.php
@@ -25,8 +25,61 @@
                 </ul>
             </div>
         </div>
-        <div class="col-md-9">
-            Chat body here.
+        <div class="col-md-9 chat-section">
+            <div class="chat-header">
+                <div class="chat-image"> 
+                    {!! makeImageFromName($user->name) !!}
+                </div>
+                <div class="chat-name font-weight-bold"> 
+                    {{ $user->name }}
+                    <i class="fa fa-circle user-status-head" title="away" id="userStatusHead{{$friendInfo->id}}"></i>
+                </div>
+            </div>
+
+            <div class="chat-body" id="chatBody">
+                <div class="message-listing" id="messageWrapper">
+                    <div class="row message align-items-center mb-2">
+                        <div class="col-md-12 user-info">
+                            <div class="chat-image">
+                                {!! makeImageFromName('Najmul Ovi') !!}
+                            </div>
+
+                            <div class="chat-name font-weight-bold">
+                                Najmul Ovi
+                                <span class="small time text-gray-500" title="2021-04-15 10:30 PM">
+                                    10:30 PM
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12 message-content">
+                            <div class="message-text">
+                                Message Here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat-box">
+                <div class="chat-input bg-white" id="chatInput" contenteditable="">
+
+                </div>
+
+                <div class="chat-input-toolbar">
+                    <button title="Add File" class="btn btn-light btn-sm btn-file-upload">
+                        <i class="fa fa-paperclip"></i>
+                    </button> | 
+
+                    <button title="Bold" class="btn btn-light btn-sm tool-items" onclick="document.execCommand('bold', false, '')">
+                        <i class="fa fa-bold tool-icon"></i>
+                    </button>
+
+                    <button title="Italic" class="btn btn-light btn-sm tool-items" onclick="document.execCommand('italic', false, '')">
+                        <i class="fa fa-italic tool-icon"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
